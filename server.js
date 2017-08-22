83303abe8ce0e59b43c9204880882697d7ee0e80
svemulapalli96 <svemulapalli96@gmail.com>
2017-08-22 22:43:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.get('/ui/main.js', function(req,res){
 });
 
 var counter = 0;
-app.get('/counter', function(req,res){
+app.get('/counter1', function(req,res){
     counter = counter + 1;
     res.send(counter.toString());
 });
