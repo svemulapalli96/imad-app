5fb0b4b1bc9eebc63557b6305211c2f6c4fec747
svemulapalli96 <svemulapalli96@gmail.com>
2017-08-22 22:01:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 
 var element = document.getElementById('sasi');
 element.innerHTML = "Shashikanth edited the Javascript in the console";
+
+var img = document.getElementById('image');
+img.onClick = function()
+{
+    img.style.marginLeft = '100px';
+};
