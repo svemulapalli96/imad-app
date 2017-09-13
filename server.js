76a60565ce5c069eb4362a04e91f46515cbfa38b
svemulapalli96 <svemulapalli96@gmail.com>
2017-09-13 20:24:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ app.get('/counter', function(req,res){
 
 var pool = new Pool(config)
 app.get('/test-db',function(req,res){
-    pool.query('SELECT * FROM test',function(err,res){
+    pool.query('SELECT * FROM test',function(err,result){
         if(err){
             res.status(500).send(err.toString());
         }
